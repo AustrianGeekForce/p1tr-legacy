a57cbf3e992c2cb5ed94c7a9c2dd69f3aad7e50a
Christian Ortner <chris.ortner@gmail.com>
2012-07-11 19:09:27 +0200

    Some experimentation with oyoyo.

--- p1tr/p1tr.py
+++ p1tr/p1tr.py
@@ -3,13 +3,26 @@
 import sys
 from oyoyo.client import IRCClient
 from oyoyo.cmdhandler import DefaultCommandHandler
-
+from oyoyo import helpers
 
 class MyHandler(DefaultCommandHandler):
     def privmsg(self, nick, chan, msg):
         print("%s in %s said: %s" % (nick, chan, msg))
 
 
+def on_connect(client):
+    helpers.join(client, '#p1tr-test')
+
 def main():
-    print(sys.args)
-    client = IRCClient(MyHandler, host='irc.freenode.net', port=6667, nick='p1tr-test')
+    client = IRCClient(MyHandler, host='irc.freenode.net', port=6667, nick='p1tr-test', connect_cb=on_connect)
+    connection = client.connect()
+    while True:
+        try:
+            next(connection) 
+        except KeyboardInterrupt:
+            break
+
+
+
+if __name__ == '__main__':
+    main()
